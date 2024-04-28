96c4ec64a9ebe95779001d41a7f2f59d1e8b47be
LouisCoutel <louiscoutel75@gmail.com>
2024-04-28 17:51:04 +0200

    write annotations for messages and colors

--- ui/colors.lua
+++ ui/colors.lua
@@ -1,4 +1,4 @@
-
+---@type table<color, string> RGB color values formatted for ANSI SGR
 COLORS = {
 	pink = "246;114;242",
 	white = "252;247;248",
@@ -7,8 +7,14 @@ COLORS = {
 	green = "41;191;18",
 }
 
+---@alias utility function
+
+---Surround string with ANSI SGR arguments to apply color
+---@type utility
+---@param text string Text to color
+---@param color string Color name to look up in COLORS table
+---@return string text
 function SET_COLOR(text, color)
-	print(COLORS[color])
 	if COLORS[color] ~= nil then
 		return "\27[38;2;" .. COLORS[color] .. "m" .. text .. "\27[0m"
 	else
--- ui/messages.lua
+++ ui/messages.lua
@@ -1,8 +1,16 @@
 require("ui.colors")
 
-LAZERTAG = SET_COLOR("<LAZER> ", "pink")
+---@type string
+---@see SET_COLOR
+LAZERTAG = SET_COLOR("<LAZER>", "pink") .. ": "
+
 LB = "\n"
 
+--- Add app tag and line break to string and set text color
+---@type utility
+---@param text string Text to format
+---@param text_color string Color to apply
+---@return string message Formatted and colored message
 function CREATE_MSG(text, text_color)
 	local body = SET_COLOR(text, text_color)
 	return LAZERTAG .. body .. LB
